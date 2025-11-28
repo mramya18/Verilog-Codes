c24eb5426d66df1460b552a0f9a0946862294569
mramya18 <madhiraramya18@gmail.com>
2025-11-28 22:06:44 +0530

    Add files via upload

---
+++ encoder_4to2_tb.v
@@ -0,0 +1,19 @@
+module encoder_4to2_tb;
+
+    reg  [3:0] d;
+    wire [1:0] y;
+
+    encoder_4to2 dut(.d(d), .y(y));
+
+    initial begin
+
+        d = 4'b0001; #10;
+        d = 4'b0010; #10;
+        d = 4'b0100; #10;
+        d = 4'b1000; #10;
+
+
+        $finish;
+    end
+
+endmodule
